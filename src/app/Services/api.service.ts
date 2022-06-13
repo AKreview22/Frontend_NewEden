e82f8c2cb83542b6a46ae0fc1ff6405be10bca11
SarahhAymann <sarahayman131@gmail.com>
2022-06-14 00:25:35 +0200

    Api

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -121,15 +121,15 @@ export class ApiService {
     return this.http.get<any>("http://localhost:3000/sum");
   }
   postSellData(data: any) {
-    return this.http.post<any>("http://localhost:3000/sellDetails/", data);
+    return this.http.post<any>("http://127.0.0.1:8000/Home/sell/", data);
   }
   postCalcPayment(data: any){
-    return this.http.post<any>("http://127.0.0.1:8000/Home/calc/",data);
+    return this.http.post<any>("http://127.0.0.1:8000/EdenApp/calc/",data);
   }
 
   getPredictionResult()
   {
-    return this.http.get("http://127.0.0.1:8000/Home/predictionResult/")
+    return this.http.get("http://127.0.0.1:8000/EdenApp/predictionResults/")
   }
 
 
@@ -154,20 +154,9 @@ export class ApiService {
     );
   }
 
-  getPredictionResults(): Observable<IResults[]> {
-    return this.http.get('http://localhost:3000/predictionDetails').pipe(
-      map(res => {
-        const resultsArray: Array<IResults> = [];
-        for (const id in res) {
-          resultsArray.push(res[id]);
-        }
-        return resultsArray;
-      })
-    );
-  }
 
   postPredictionResults(data: any) {
-    return this.http.post<any>("http://localhost:3000/predictionDetails", data);
+    return this.http.post<any>("http://127.0.0.1:8000/EdenApp/pricePrediction/", data);
   }
 
 
