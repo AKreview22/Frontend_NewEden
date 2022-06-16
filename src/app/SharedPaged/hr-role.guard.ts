4f0033d7ab20bbc9d31122c2afe2244402da7624
MartinaNaeem <martina9naeem@gmail.com>
2022-06-16 20:51:49 +0200

    commit

--- src/app/SharedPaged/hr-role.guard.ts
+++ src/app/SharedPaged/hr-role.guard.ts
@@ -16,9 +16,9 @@ export class HrRoleGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.service.isLoggedIn()) {
       this.currentRole = this.service.GetRoleByToken(this.service.getToken());
-      if (this.currentRole == "HR"||this.currentRole == "Superuser") {
+      if (this.currentRole == "HR" ||this.currentRole == "Superuser" ) {
         return true;
-      // } else {
+      } else {
         alert('you are not authorized to access this menu');
         this.router.navigate(['']);
         return false;
